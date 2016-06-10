04c5f70b33dca8bbd8405ef5b27f28065a8475e3
lincanbin <lincanbin@hotmail.com>
2016-06-10 16:20:02 +0800

    Initialize.

---
+++ carbonforum/carbonforum/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  carbonforum
+//
+//  Created by lincanbin on 16/6/10.
+//  Copyright © 2016年 lincanbin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
